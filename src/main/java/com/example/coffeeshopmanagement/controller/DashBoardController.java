a85981571785d233be927757d3f314d1459b4e76
Tạ Thị Thanh Thủy <tathanhthuy17032004@gmail.com>
2023-09-17 21:24:28 +0700

    chart income, customer quantity

--- src/main/java/com/example/coffeeshopmanagement/controller/DashBoardController.java
+++ src/main/java/com/example/coffeeshopmanagement/controller/DashBoardController.java
@@ -4,6 +4,9 @@ import com.example.coffeeshopmanagement.database.JDBCConnect;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -11,6 +14,12 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 
 public class DashBoardController implements Initializable {
@@ -18,6 +27,11 @@ public class DashBoardController implements Initializable {
     private Label username;
     @FXML
     private Button closeBtn;
+    @FXML
+    private AreaChart<?, ?> customer_day;
+
+    @FXML
+    private BarChart<?, ?> daily_revenue;
 
     private JDBCConnect jdbcConnect;
 
@@ -61,8 +75,97 @@ public class DashBoardController implements Initializable {
         }
     }
 
+//    income for day
+    public void dashboardIncomeChart() {
+        daily_revenue.getData().clear();
+
+        // Get the current date
+        LocalDate currentDate = LocalDate.now();
+
+        // Define a date format for the SQL query
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        // Calculate the first day of the current month
+        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
+
+        String sql = "SELECT date, SUM(quantity) " +
+                "FROM customer " +
+                "WHERE date >= ? " +
+                "GROUP BY date " +
+                "ORDER BY TIMESTAMP(date)";
+
+        Connection connection = jdbcConnect.getJDBCConnection();
+        XYChart.Series chart = new XYChart.Series();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, firstDayOfMonth.format(dateFormat));
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                chart.getData().add(new XYChart.Data<>(resultSet.getString(1),
+                        resultSet.getFloat(2)));
+            }
+
+            daily_revenue.getData().add(chart);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+//customer quantity for day
+    public void dashboardCustomerChartForCurrentDay() {
+        customer_day.getData().clear();
+
+        LocalDate currentDate = LocalDate.now();
+
+        // Define a date format for the SQL query
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        // Calculate the first day of the current month
+        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
+
+        Connection connection = jdbcConnect.getJDBCConnection();
+        XYChart.Series chart = new XYChart.Series();
+
+        // Get the current year, month, and day
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1; // Month is zero-based, so we add 1
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        try {
+            String sql = "SELECT date, COUNT(DISTINCT customer_id) " +
+                    "FROM customer " +
+                    "WHERE date >= ? " +
+                    "GROUP BY date " +
+                    "ORDER BY TIMESTAMP(date)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, firstDayOfMonth.format(dateFormat));
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                chart.getData().add(new XYChart.Data<>(resultSet.getString(1),
+                        resultSet.getFloat(2)));
+            }
+
+            customer_day.getData().add(chart);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         displayUsername();
+        dashboardIncomeChart();
+        dashboardCustomerChartForCurrentDay();
     }
 }
