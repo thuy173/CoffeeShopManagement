e39fc1e856c1f72e4d6e0301f0643a7a5172b93e
Tạ Thị Thanh Thủy <tathanhthuy17032004@gmail.com>
2023-09-19 23:14:08 +0700

    display employee

--- src/main/java/com/example/coffeeshopmanagement/controller/EmployeeController.java
+++ src/main/java/com/example/coffeeshopmanagement/controller/EmployeeController.java
@@ -1,4 +1,58 @@
 package com.example.coffeeshopmanagement.controller;
 
-public class EmployeeController {
+import com.example.coffeeshopmanagement.database.JDBCConnect;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeController implements Initializable {
+
+    @FXML
+    private Button closeBtn;
+
+    private JDBCConnect jdbcConnect;
+
+    private Stage stage;
+    private StageManager stageManager = new StageManager();
+
+    public EmployeeController() {
+        this.jdbcConnect = new JDBCConnect();
+        this.stageManager = new StageManager();
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+
+    @FXML
+    void close(ActionEvent event) {
+        Alert confirmationDialog = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationDialog.setTitle("Confirmation");
+        confirmationDialog.setHeaderText("Are you sure you want to CLOSE?");
+        confirmationDialog.setContentText("Press OK to close or Cancel.");
+
+        // Show the dialog and wait for a result
+        ButtonType result = confirmationDialog.showAndWait().orElse(ButtonType.CANCEL);
+
+        // If the user clicks OK, proceed with the logout
+        if (result == ButtonType.OK) {
+            // Close the current stage
+            stage = (Stage) closeBtn.getScene().getWindow();
+            stage.close();
+
+        }
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
