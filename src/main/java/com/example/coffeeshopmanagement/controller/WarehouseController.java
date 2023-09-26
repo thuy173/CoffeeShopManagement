30b5aec594341e47321707edd924bf310fa1ae12
Tạ Thị Thanh Thủy <tathanhthuy17032004@gmail.com>
2023-09-26 14:31:59 +0700

    add item inventory

--- src/main/java/com/example/coffeeshopmanagement/controller/WarehouseController.java
+++ src/main/java/com/example/coffeeshopmanagement/controller/WarehouseController.java
@@ -1,4 +1,26 @@
 package com.example.coffeeshopmanagement.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class WarehouseController {
+    @FXML
+    private TextField ID;
+
+    @FXML
+    private Button closeBtn;
+
+    @FXML
+    private VBox displayItem;
+
+    public WarehouseController() {
+    }
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
 }
